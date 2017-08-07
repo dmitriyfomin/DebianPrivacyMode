1722e691ba81c0379a049c1bd4684821247f9fc4
user <khan@localhost.localdomain>
2017-08-07 11:44:11 +0300

    added privacy addons

--- DebianPrivacyMode.sh
+++ DebianPrivacyMode.sh
@@ -2,13 +2,13 @@
 
 # Debian privacy mode upgrade. Installing tools for privacy & security and removing some packages
 
-# FIRST install kali-anonsurf & run this script as root
+# FIRST install kali-anonsurf, start anonsurf (# anonsurf start)  & run this script (as root)
 
 
 apt update
 apt -y install apt-transport-tor
 
-# If you need GPU driver (Nvidia, Radeon, etc.), add non-free after contrib to install it
+# If you need GPU driver (Nvidia, Radeon, etc.), add non-free after contrib to install it. Change "stretch" to "jessie" or other codename of your version, or use this mirrors to upgrade from 8 Jessie.
 
 echo "# Debian .onion Tor network mirrors. More info https://onion.debian.org/ \
 
@@ -22,9 +22,8 @@ deb  tor+http://sgvtcaew4bxjd7ln.onion/debian-security stretch/updates    main c
 
 deb  tor+http://vwakviie2ienjx6t.onion/debian          stretch-backports  main contrib" > /etc/apt/sources.list
 
-apt update && apt -y dist-upgrade && apt -y install aircrack-ng apt-transport-https apt-transport-tor \
-audacity bleachbit brasero \
-build-essential gcc gcr \
+apt update && apt -y dist-upgrade && apt -y install aircrack-ng audacity bleachbit \
+brasero build-essential gcc gcr \
 cpp cpp-6 cryptsetup cryptsetup-bin \
 curl dasher dirmngr g++ gimp \
 geoip-database git \
@@ -32,15 +31,16 @@ gnupg gnupg1 gnupg2 \
 gpgv gtkhash inkscape keepassx libgpg-error0 \
 libgpgme11 hopenpgp-tools libgpgmepp-doc \
 libssh-gcrypt-4 liferea macchanger \
-make mat net-tools nmap \
+macchanger-gtk make mat net-tools nmap \
 onioncircuits openpgp-applet openssl \
 pidgin pitivi seahorse \
 scdaemon tor torchat transmission \
 traverso vlc whois wireshark \
-xul-ext-torbirdy xul-ext-ublock-origin zip \
+xul-ext-https-everywhere xul-ext-torbirdy xul-ext-ublock-origin \
+xul-ext-noscript xul-ext-self-destructing-cookies zip \
 zulucrypt-cli zulucrypt-gui
 
-# or remove packages from this list manually. If you don't use your workstation as file storage server, FTP-server, Telnet-server, etc., remove it. For example: # apt remove jboss-autopwn
+# or remove packages from this list manually. If you don't use your workstation as file storage server, FTP-server, Telnet-server, etc., remove it. For example: # apt remove jboss-autopwn samba
 apt -y remove avahi-daemon cups minissdpd \
 nfspy rpcbind samba samba-common telnet \
 tracker virt-viewer vlc-plugin-samba && \
